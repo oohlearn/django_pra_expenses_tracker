1ac5e9cba9b8ba8d6f372fa32cd4b317536e3525
oohlearn <learnooh@gmail.com>
2024-05-25 14:47:02 +0800

    add delete and edit btn

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.list_view, name="list")
+    path("", views.list_view, name="list"),
+    path("edit/<int:id>", views.edit, name="edit"),
+    path("delete/<int:id>", views.delete, name="delete"),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Expense
+from .forms import ExpenseForm
 
 # Create your views here.
 
 
 def list_view(request):
     expenses = Expense.objects.all()
-    return render(request, 'myapp/index.html', {'expenses': expenses})
+    form = ExpenseForm()
+    if request.method == "POST":
+        expense = ExpenseForm(request.POST)
+        expense.save()
+        return redirect("/")
+
+    return render(request, 'myapp/index.html', {'expenses': expenses,
+                                                "form": form})
+
+
+def edit(request, id=id):
+    expense = Expense.objects.get(id=id)
+    form = ExpenseForm(instance=expense)
+    if request.method == "POST":
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    return render(request, 'myapp/edit.html', {'form': form})
+
+
+def delete(request, id=id):
+    if request.method == "POST" and "delete" in request.POST:
+        expense = Expense.objects.get(id=id)
+        expense.delete()
+    return redirect("/")
